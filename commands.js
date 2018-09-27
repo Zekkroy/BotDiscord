7da8fa9a6c7bc8e577b7d5b457b1c090d87bb8b5
Zekkroy <37773965+Zekkroy@users.noreply.github.com>
2018-09-27 02:46:49 +0200

    Delete commands.js

--- commands.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on("message", (message) => {
-  if (message.content == (prefix + "hola")) {
-    message.channel.send("Qui ti pasa");
-  	}
-});
