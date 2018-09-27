dc9672c500290610a851d7a5776e1a56047c10d7
Zekkroy <37773965+Zekkroy@users.noreply.github.com>
2018-09-27 03:09:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,6 @@ client.on("message", (message) => {
   	}
 });
 
-if (message.content == (prefix + "ayuda")) {
-    message.channel.send({embed: {
-      color: 3447003,
-      description: "Este bot está en pleno desarrollo. La variedad de comandos es muy escasa actualmente, se irán añadiendo nuevos progresivamente. :robot:."
-    }})
-};
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
