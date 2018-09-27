a6b5389b1b50d9361846bb7d57a364e6c1ad6700
Zekkroy <37773965+Zekkroy@users.noreply.github.com>
2018-09-27 02:38:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const commands = require("./commands.json");
 
 client.on('ready', () => {
     console.log('I am ready!');
